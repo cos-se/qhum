7bf05afde7e6ea14d1fb403912239b885dffbac5
cos-se <tamas.marki@svenskakyrkan.se>
2016-09-19 16:53:02 +0200

    v2.062
    added touchwipe

--- v2/js/functions.js
+++ v2/js/functions.js
@@ -1339,6 +1339,18 @@ alasql.promise('SELECT * FROM XLSX("'+xlsxurl+'",{sheetid:"Grants"})'+ (showLast
 		cmdInput.select(); // autoselect textarea
 	}; // END OF CONSOLE
 
+	if(is_iPhone){
+		//jQuery Touchwipe Plugin by Andreas Waltl http://www.netcu.de/jquery-touchwipe-iphone-ipad-library
+		(function($){$.fn.touchwipe=function(settings){var config={min_move_x:20,min_move_y:20,wipeLeft:function(){},wipeRight:function(){},wipeUp:function(){},wipeDown:function(){},preventDefaultEvents:true};if(settings)$.extend(config,settings);this.each(function(){var startX;var startY;var isMoving=false;function cancelTouch(){this.removeEventListener('touchmove',onTouchMove);startX=null;isMoving=false}function onTouchMove(e){if(config.preventDefaultEvents){e.preventDefault()}if(isMoving){var x=e.touches[0].pageX;var y=e.touches[0].pageY;var dx=startX-x;var dy=startY-y;if(Math.abs(dx)>=config.min_move_x){cancelTouch();if(dx>0){config.wipeLeft()}else{config.wipeRight()}}else if(Math.abs(dy)>=config.min_move_y){cancelTouch();if(dy>0){config.wipeDown()}else{config.wipeUp()}}}}function onTouchStart(e){if(e.touches.length==1){startX=e.touches[0].pageX;startY=e.touches[0].pageY;isMoving=true;this.addEventListener('touchmove',onTouchMove,false)}}if('ontouchstart'in document.documentElement){this.addEventListener('touchstart',onTouchStart,false)}});return this}})(jQuery);
+
+		$('ul#projects').touchwipe({
+			wipeRight: function() { $('#projects>li').addClass('on'); },
+			wipeLeft: function() { $('#projects>li').removeClass('on'); },
+			min_move_x: 20,
+			min_move_y: 20,
+			preventDefaultEvents: true
+		});
+	};
 	
 	// Monitor source xlsx for changes
 	if(!is_iPhone && dropboxMonitor) {
