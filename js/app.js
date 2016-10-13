963cfe8d4ceefc4f46f8cc45a00ac973a1d86304
cos-se <tamas.marki@svenskakyrkan.se>
2016-10-13 17:58:29 +0200

    7

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
 var is_iPhone = /iPhone|iPod|iPhone Simulator/.test(navigator.platform);
 
 var setup = {
-	xlsxurl: 'https://dl.dropboxusercontent.com/s/wrylh81p763xym8/cos-hum_grants_since_2000.xlsx?dl=0',
+	xlsxurl: 'https://dl.dropboxusercontent.com/u/2624323/cos/cos-hum_grants_since_2000_BACKUP.xlsx',
 	googleMapsApiKey: 'AIzaSyDo-siqnczOSWCRoUEygoTySkDUsSsX-ak',
 	googleMapsGeocodingKey: 'AIzaSyDs3bo2R4NPqiU0geRF7ZOEtsx_KDWZSPU',
 	dropboxAccessToken: 'aespR2ILdtAAAAAAAAAHEl6pViZWzZAt3JqBkjfGJORg9yANRQZrM9ROpBbihdgQ',
