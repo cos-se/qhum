1900c4776c222a172978b6ae7f11d3f885e2aa5e
cos-se <tamas.marki@svenskakyrkan.se>
2016-10-13 16:47:26 +0200

    VERSION 2

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ $(function() {
 var is_iPhone = /iPhone|iPod|iPhone Simulator/.test(navigator.platform);
 
 var setup = {
-	xlsxurl: 'https://dl.dropboxusercontent.com/u/2624323/cos/qh2/test2.xlsx',
+	xlsxurl: 'https://dl.dropboxusercontent.com/s/wrylh81p763xym8/cos-hum_grants_since_2000.xlsx',
 	googleMapsApiKey: 'AIzaSyDo-siqnczOSWCRoUEygoTySkDUsSsX-ak',
 	googleMapsGeocodingKey: 'AIzaSyDs3bo2R4NPqiU0geRF7ZOEtsx_KDWZSPU',
 	dropboxAccessToken: 'aespR2ILdtAAAAAAAAAHEl6pViZWzZAt3JqBkjfGJORg9yANRQZrM9ROpBbihdgQ',
-	dropboxFileId: 'id:wRpyqQla8qgAAAAAAAAytQ',
+	dropboxFileId: 'id:QegsPur5FeAAAAAAAAAAAQ',
 	dropboxMonitor: [30, 0], // in seconds, first desktop, second mobile (0 if false)
 	vipsImg: 'http://vips.svenskakyrkan.se/_layouts/15/Images/Precio.NGO.UI/layout/logo.png', // this image will be checked to see if the user has access to Vips (intranet)
 	RP1417: ['500364', '500134', '500101', '500094', '500102', '500344', '500785', '500786'], // these are the Vips ID numbers of the projects that belong to the Refugee Programme 2014-2017
@@ -20,7 +20,7 @@ var setup = {
 	}
 };
 
-// id:VAmjyVf2lRAAAAAAAAAAAQ // test.xlsx
+// id:VAmjyVf2lRAAAAAAAAAAAQ // test.xlsx https://dl.dropboxusercontent.com/u/2624323/cos/qh2/test2.xlsx
 // id:wRpyqQla8qgAAAAAAAAytQ // test2.xlsx
 // id:QegsPur5FeAAAAAAAAAAAQ // CoS grants
 // console.log(dbx.filesListFolder({path: '/Public/cos/qh2/'})); // For finding Dropbox file IDs (paths)
