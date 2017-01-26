af3dd1b15158452ce21913f966991f8448e2b5e0
cos-se <tamas.marki@svenskakyrkan.se>
2017-01-26 11:27:51 +0100

    60

--- js/app.js
+++ js/app.js
@@ -775,8 +775,8 @@ function loadDOM() {
 									.append($('<div class="menu"><span class="menuitem" title="Show grants from the last 3 months"><svg fill="#FFFFFF" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/><path d="M0 0h24v24H0z" fill="none"/></svg></span></div>')
 										.on(tap, function(e) {
 											e.preventDefault(e);
-											var last3months = alasql('SELECT id, code, date_decision, '+ list.columnCostCentres.join(', ') +', country, partner_name, link_db 	 FROM grant WHERE date_decision > DATEADD(month, -3, GETDATE()) ORDER BY date_decision'),
-												date3monthsAgo = Date.parse(alasql('SELECT VALUE DATEADD(month, -3, GETDATE())')),
+											var last3months = alasql('SELECT id, code, date_decision, '+ list.columnCostCentres.join(', ') +', country, partner_name, link_db 	 FROM grant WHERE date_decision > DATEADD(month, -3, NOW()) ORDER BY date_decision'),
+												date3monthsAgo = Date.parse(alasql('SELECT VALUE DATEADD(month, -3, NOW())')),
 												table = $('<table/>',{'class': 'grantlist'}).append('<caption>The table below shows decisions since '+ moment(date3monthsAgo).format('D MMMM YYYY') +'</caption><tr><th>Decision</th><th>Project</th><th>Partner</th><th>Country</th><th>Donor</th><th>Amount</th></tr>'),
 												content = $('<div/>',{'style': 'padding: 20px;'}).append(table);
 											for (var i = 0; i < last3months.length; i++) {
